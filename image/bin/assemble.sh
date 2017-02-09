9bca38009aa6f7f1108b6f01adcfaf784e2e7491
Michael Barton <mail@michaelbarton.me.uk>
2017-02-09 14:57:15 -0800

    Minor fixes to get image in final working shape

--- image/bin/assemble.sh
+++ image/bin/assemble.sh
@@ -8,3 +8,17 @@ READS=$(biobox_args.sh 'select(has("fastq")) | .fastq | map("--12 \(.value)") | 
 
 cd $(mktemp -d)
 gatb ${READS}
+
+cp assembly.fasta ${OUTPUT}/contigs.fa
+
+cat << EOF > ${OUTPUT}/biobox.yaml
+version: 0.9.0
+arguments:
+  - fasta:
+    - id: contigs_1
+      value: contigs.fa
+      type: contigs
+EOF
+
+# Clean up all generated files
+rm -rf -- "$(pwd -P)"
--- image/bin/install.sh
+++ image/bin/install.sh
@@ -7,9 +7,23 @@ set -o xtrace
 
 export PATH=${PATH}:/usr/local/bin/install
 
-NON_ESSENTIAL_BUILD="make ca-certificates wget gcc g++ python-pip python-dev zlib1g-dev"
-ESSENTIAL_BUILD="gfortran libopenblas-dev"
-RUNTIME="python-minimal bwa"
+NON_ESSENTIAL_BUILD="
+	ca-certificates \
+	g++ \
+	gcc \
+	gfortran \
+	libfreetype6-dev \
+	libopenblas-dev \
+	libxft-dev \
+	make \
+	python-dev \
+	python-pip \
+	wget \
+	zlib1g-dev \
+	"
+
+ESSENTIAL_BUILD=""
+RUNTIME="python-minimal libpython-stdlib bwa libfreetype6 libopenblas-base"
 
 # Build dependencies
 apt-get update --yes
@@ -21,11 +35,9 @@ besst.sh
 pip install --user --requirement /usr/local/share/python_requirements.txt
 
 # Clean up dependencies
-#apt-get autoremove --purge --yes ${NON_ESSENTIAL_BUILD}
-#apt-get clean
+apt-get autoremove --purge --yes ${NON_ESSENTIAL_BUILD}
+apt-get clean
 
 # Install required files
 apt-get install --yes --no-install-recommends ${RUNTIME}
-#rm -rf /var/lib/apt/lists/*
-
-# Can fortan libopenblas dependencies be removed?
+rm -rf /var/lib/apt/lists/*
